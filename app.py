d913dad55364aa1514ae2246cdeefa134535f248
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-03-24 17:37:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,20 +30,20 @@ def text_to_speech(text, tld):
 
 # Recetas predefinidas
 recetas = {
-    "Tacos de Pollo Mexicano": {
-        "Descripción": "Deliciosos tacos de pollo con un toque mexicano.",
-        "Ingredientes": "- Pollo desmenuzado\n- Tortillas de maíz\n- Cebolla\n- Tomate\n- Aguacate\n- Cilantro\n- Limón\n- Salsa de tomate\n- Especias mexicanas",
-        "Instrucciones": "1. Saltear el pollo con las especias.\n2. Calentar las tortillas.\n3. Servir con los ingredientes frescos y salsa."
+    "Receta 1": {
+        "Descripción": "Descripción de la Receta 1",
+        "Ingredientes": "Ingredientes de la Receta 1",
+        "Instrucciones": "Instrucciones de la Receta 1"
     },
-    "Espaguetis a la Boloñesa": {
-        "Descripción": "Clásicos espaguetis con una deliciosa salsa de carne italiana.",
-        "Ingredientes": "- Espaguetis\n- Carne molida\n- Cebolla\n- Ajo\n- Zanahoria\n- Salsa de tomate\n- Vino tinto\n- Aceite de oliva\n- Especias italianas",
-        "Instrucciones": "1. Cocinar la carne con las verduras.\n2. Agregar la salsa de tomate y el vino.\n3. Cocinar a fuego lento.\n4. Servir sobre los espaguetis."
+    "Receta 2": {
+        "Descripción": "Descripción de la Receta 2",
+        "Ingredientes": "Ingredientes de la Receta 2",
+        "Instrucciones": "Instrucciones de la Receta 2"
     },
-    "Salmón al Horno con Verduras": {
-        "Descripción": "Salmón fresco horneado con una variedad de verduras sazonadas.",
-        "Ingredientes": "- Filetes de salmón\n- Brócoli\n- Zanahorias\n- Calabacín\n- Limón\n- Aceite de oliva\n- Sal\n- Pimienta\n- Hierbas aromáticas",
-        "Instrucciones": "1. Colocar el salmón y las verduras en una bandeja.\n2. Rociar con aceite de oliva y especias.\n3. Hornear hasta que estén dorados y tiernos."
+    "Receta 3": {
+        "Descripción": "Descripción de la Receta 3",
+        "Ingredientes": "Ingredientes de la Receta 3",
+        "Instrucciones": "Instrucciones de la Receta 3"
     }
 }
 
@@ -76,3 +76,4 @@ def remove_files(n):
                 os.remove(f)
 
 remove_files(7)
+
